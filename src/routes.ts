b27446291ff4dc506d2764ed0206333183d64dad
MARCO AURELIO <marcocpdti@gmail.com>
2021-06-14 22:48:19 -0300

    add routes configuration

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Request, Response, Router } from 'express';
+import userController from './controllers/UserController';
+import sessionController from './controllers/SessionController';
 
 import AuthMiddleware from './middlewares/auth';
 
@@ -9,6 +11,15 @@ routes.get('/', (req: Request, res: Response) => {
     return res.json({message: 'Hello World!'});
 });
 
+//User routes not authenticated
+routes.post('/users', userController.create);
+routes.post("/sessions", sessionController.store);
+
 routes.use(AuthMiddleware);
+//User routes authenticated
+routes.get('/users', userController.getAll);
+routes.get('/users/:id', userController.getById);
+routes.get('/users/:email', userController.getByEmail);
+routes.put('/users/:id', userController.update);
 
 export default routes;
