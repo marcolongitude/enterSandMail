83b935896fa5b3d9764bdf7db83b10bc833c4151
MARCO AURELIO <marcocpdti@gmail.com>
2021-06-24 21:49:02 -0300

    gitflow-feature-stash: addFrontReactWebTS

--- src/util/readFileXLS.ts
+++ src/util/readFileXLS.ts
@@ -5,12 +5,13 @@ interface IXlData {
   Email: string;
   Telefone: number;
 }
+
 export const readFileXLS = (pathFileXLXS: string): Array<object> => {
   var workbook = xlsx.readFile(pathFileXLXS);
   var sheet_name_list = workbook.SheetNames;
   var xlData: Array<IXlData> = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
-  let response: Array<object> = []
+  let response: Array<object> = [];
 
   xlData.forEach(element => {
     response.push({
