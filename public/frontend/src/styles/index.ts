f523440fd5d6d93cd33ae220ba95e67c72d31993
MARCO AURELIO <marcocpdti@gmail.com>
2021-06-24 23:34:55 -0300

    add css component colors

---
+++ public/frontend/src/styles/index.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  blueActive: '#0077b6',
+
+  textTitle: '#ced4da',
+  textErrorInput: '#b23a48',
+  placeholder: 'rgba(255, 255, 255, 0.8),',
+
+  backgroundInput: 'white',
+  backgroundColorDefault: '#F3F4F7'
+};
