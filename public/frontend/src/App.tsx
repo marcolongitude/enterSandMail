ee0003772f0c301765b8e9b524d6d91a6b17edad
MARCO AURELIO <marcocpdti@gmail.com>
2021-06-27 16:39:33 -0300

    replacing react router dom with a library component

--- public/frontend/src/App.tsx
+++ public/frontend/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import store from './store';
 
-import Login from './pages/login'
+import Router from './router';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Login />
-      </div>
+      <Router />
     </Provider>
   );
 }
