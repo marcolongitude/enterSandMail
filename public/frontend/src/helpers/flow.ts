4c6e18879b0677b612fbcf42f71a0a93d88c6a5e
MARCO AURELIO <marcocpdti@gmail.com>
2021-06-27 17:20:15 -0300

    add type props

--- public/frontend/src/helpers/flow.ts
+++ public/frontend/src/helpers/flow.ts
@@ -1,6 +1,6 @@
 import { ROUTES } from "../constants"; 
 
-const goTo = (path: any, params: any = false) => {
+const goTo = (path: string, params: boolean = false) => {
   window.location.replace(
     decodeURIComponent((path || ROUTES.HOME) + (params ? params : "")) 
   );
