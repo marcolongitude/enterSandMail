c29555fb74bafada2b2e3b4dad216cbf7bdd1199
MARCO AURELIO <marcocpdti@gmail.com>
2021-06-27 16:39:44 -0300

    Initial component navbar

---
+++ public/frontend/src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import { ROUTES } from "../constants";
+
+import { flowService } from "../helpers/flow";
+
+const NavBar = () => {
+  const handleLogout = () => {
+    localStorage.removeItem("reduxState"); 
+    flowService.goTo(ROUTES.LOGIN); 
+  };
+
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to={ROUTES.HOME}>Home</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.ABOUT}>About</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.CONTACT}>Contact</Link>
+        </li>
+        <li>
+          <a href="/" onClick={handleLogout}>
+            Logout
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default NavBar;
