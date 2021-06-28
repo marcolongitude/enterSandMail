40f18c2ad17fb1634d8465e5ed4e40dfbea814b2
MARCO AURELIO <marcocpdti@gmail.com>
2021-06-28 00:50:48 -0300

    initial replace function validate form

--- public/frontend/src/pages/Login/index.tsx
+++ public/frontend/src/pages/Login/index.tsx
@@ -57,6 +57,9 @@ const useStyles = makeStyles({
   },
   title: {
     paddingBottom: 10
+  },
+  errorInput: {
+    color: '#922a2a'
   }
 })
 
@@ -72,8 +75,10 @@ export const Login = (): JSX.Element => {
   const dispatch = useDispatch();
   const classes = useStyles()
   const [isAuth, setIsAuth] = useState(false);
-  const [hasError, setHasError] = useState(false); 
+  const [hasError, setHasError] = useState({ isError: false, message: ""}); 
   const [formState, setFormState] = useState({ user_email: "", password: "" });
+
+  const [errorsState, setErrorsState] = useState({fields: {user_email: '', password: ''}, errors: {user_email: '', password: ''} })
 
   useEffect(() => {
     const isAuth = localStorage.getItem("reduxState");
@@ -89,23 +94,67 @@ export const Login = (): JSX.Element => {
       ...formState,
       [name]: value 
     });
+
   };
 
   const handleSubmit = async (event: { preventDefault: () => void }) => {
     event.preventDefault(); 
-    let response: IResponse = await sessionUser('sessions', formState); 
-    
-    if(response.data){
-      dispatch(loginUser(response.data))
-    }else{
-      console.log(response.message, response.status)
-      setHasError(true);
-      return;
+
+    if(handleValidation()){
+      let response: IResponse = await sessionUser('sessions', formState); 
+      
+      if(response.data){
+        dispatch(loginUser(response.data))
+      }else{
+        console.log(response.message, response.status)
+        setHasError({ isError: true, message: "Usuário ou senha inválidos"});
+        return;
+      }
+  
+      flowService.goTo(ROUTES.HOME);
     }
 
-    flowService.goTo(ROUTES.HOME);
   };
 
+  const handleValidation = (): boolean => {
+    interface IErrors {
+      user_email: string;
+      password: string
+    }
+    interface IFields {
+      user_email: string;
+      password: string
+    }
+
+    let fields: IFields = formState;
+    let errors: IErrors = {user_email: '', password: ''};
+    let formIsValid = true;
+
+    //Name
+    if(!fields["password"]){
+      formIsValid = false;
+      errors["password"] = "Este campo é obrigatório";
+    }
+
+    //Email
+    if(typeof fields["user_email"] !== "undefined"){
+      let lastAtPos = fields["user_email"].lastIndexOf('@');
+      let lastDotPos = fields["user_email"].lastIndexOf('.');
+
+      if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["user_email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["user_email"].length - lastDotPos) > 2)) {
+        formIsValid = false;
+        errors["user_email"] = "Digite um email válido";
+      }
+    }
+
+    if(!fields["user_email"]){
+      formIsValid = false;
+      errors["user_email"] = "Este campo é obrigatório";
+    }
+
+    setErrorsState({fields: {user_email: fields["user_email"], password: fields["password"]}, errors: errors});
+    return formIsValid;
+  }
 
   return (
     <Container className={classes.root}> 
@@ -116,22 +165,40 @@ export const Login = (): JSX.Element => {
           <h3 className={classes.title}>Enter SandMail</h3>
           <img className={classes.logo} src={logoSandMail} alt='logo sand mail'/>
           <div className={classes.containerComponentsLogin}>
-              {hasError && <p>Login or pass invalid</p>}{" "}
+              {hasError.isError && <p>{ hasError.message }</p>}{" "}
               <TextField 
+                error={hasError.isError}
+                required
+                id="standard-required"
                 className={classes.input} 
                 variant="outlined" 
-                label="Usuário" 
+                label="E-mail" 
                 name="user_email"
                 onChange={ handleChange }  
+                value={formState["user_email"]}
               />
+              <span className={classes.errorInput}>{errorsState.errors["user_email"]}</span>
               <TextField 
+                error={hasError.isError}
+                required
+                type="password"
+                id="standard-required"
                 className={classes.input} 
                 variant="outlined" 
                 label="Senha" 
                 name="password"
                 onChange={ handleChange }
+                value={formState["password"]}
               />
-              <Button onClick={ handleSubmit } size="large" className={classes.buttonLogin} variant="contained" color="primary"> Entrar </Button>
+              <span className={classes.errorInput}>{errorsState.errors["password"]}</span>
+              <Button 
+                onClick={ handleSubmit } 
+                //onClick={ () => {myRef.current.reportValidity()}} 
+                size="large" className={classes.buttonLogin} 
+                variant="contained" 
+                color="primary"
+                > Entrar 
+              </Button>
           </div>
         </Box>
       )}
