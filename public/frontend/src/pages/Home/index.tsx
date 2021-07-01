6a0771d5ffb50525c5dc67950f42a3fdbf3ee694
MARCO AURELIO <marcocpdti@gmail.com>
2021-06-30 21:46:09 -0300

    add page home

--- public/frontend/src/pages/Home/index.tsx
+++ public/frontend/src/pages/Home/index.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { Container, BoxOption, TitleBoxOption, TextBoxOption, AreaDragFile, ButtonSubmit, Title } from './styles'
+
 
 export const Home = () => {
   return (
-    <div>Home page</div>
+    <Container>
+      {/* <Title>Escolha uma opção</Title> */}
+      <BoxOption>
+        <TitleBoxOption>
+          Carregar arquivo excel xlsx
+        </TitleBoxOption>
+        <AreaDragFile>
+          <TextBoxOption>
+            Arraste o arquivo para iniciar o envio
+          </TextBoxOption>
+          <ButtonSubmit>Selecione um arquivo para carregar</ButtonSubmit>
+        </AreaDragFile>
+      </BoxOption>
+      <BoxOption>
+        <TitleBoxOption>
+            Carregar dados em banco
+        </TitleBoxOption>
+        <AreaDragFile>
+          <TextBoxOption>
+            Dados salvos no banco de dados
+          </TextBoxOption>
+          <ButtonSubmit>Carregar dados</ButtonSubmit>
+        </AreaDragFile>
+      </BoxOption>
+    </Container>
   )
 }
 
