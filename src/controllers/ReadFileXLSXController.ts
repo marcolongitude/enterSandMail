7fa38b198ea96e3ac1447aeb4010c2eefe128a43
MARCO AURELIO <marcocpdti@gmail.com>
2021-07-06 00:23:09 -0300

    clean code

--- src/controllers/ReadFileXLSXController.ts
+++ src/controllers/ReadFileXLSXController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import {readFileXLS} from '../util/readFileXLS';
-import fs from 'fs'
 
 import multer from 'multer'
 import path from 'path'
@@ -22,25 +21,27 @@ const upload = multer({
   limits: { fileSize: maxSize },
   fileFilter: async function(req, file: Express.Multer.File, cb: (error?: any, info?: Partial<Express.Multer.File> | boolean) => void
   ){
-    const filetypes = /xlsx|xls|csv|sheet|openxmlformats|spreadsheetml/;
-    const mimetype = filetypes.test(file.mimetype);
-    const extname = path.extname(file.originalname).toLowerCase();
+    const filetypes = /xlsx|xls|csv|sheet|openxmlformats|spreadsheetml/
+    const mimetype = filetypes.test(file.mimetype)
+    const extname = path.extname(file.originalname).toLowerCase()
     
     if (mimetype && extname) {
-      return cb(null, true);
+      return cb(null, true)
     }
-    return cb(`Error: File upload only supports the following filetypes - ${filetypes}`);
+    return cb(`Error: File upload only supports the following filetypes - ${filetypes}`)
   } 
-}).single("fileXLSX"); 
+}).single("fileXLSX")
 
 
 class ReadFileXLSXReadController{
   async ReadFileXLSX(req: Request, res: Response){
     try {
 
+      
       upload(req, res, async function (err) {
         if (err) {
-          return res.status(500).json({ error: err, message: 'erro de tipo de arquivo' });
+          console.log(err)
+          return res.status(500).json({ error: err, message: 'erro de tipo de arquivo' })
         } else {
           const response = readFileXLS( pathRoot.path + '/uploads/dadosUsina.xlsx')
           return res.status(200).json({ data: response, message: 'Arquivo carregado com sucesso', status: 200 })
