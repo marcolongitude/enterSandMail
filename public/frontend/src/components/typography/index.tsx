c0e2ab6bd473bd1034b394e58f24351e16cee64b
MARCO AURELIO <marcocpdti@gmail.com>
2021-07-17 16:55:38 -0300

    add text for buttons component

--- public/frontend/src/components/typography/index.tsx
+++ public/frontend/src/components/typography/index.tsx
@@ -3,19 +3,20 @@ import styled, {css} from 'styled-components'
 interface IProps {
   color?: keyof typeof theme.colors
   size?: keyof typeof theme.sizes
-  fontWeight?: 100 | 400 | 700
+  fontWeight?: 100 | 400 | 500 | 700
   level?: 1 | 2 | 3 | 4 | 5 | 6
 }
 
 const theme = {
   colors: {
-    white: '#eee',
+    white: '#ffffff',
     black: '#111',
     gray: '#333',
     lightGray: 'CCC',
     blue: '#0077b6'
   },
   sizes: {
+    exsmall: '0.9rem',
     xsmall: '1rem',
     small: '1.2rem',
     medium: '1.6rem',
@@ -32,6 +33,21 @@ export const Text = styled('h1').attrs<IProps>(({ level = 6}) => ({
     color = 'white',
     size = 'medium',
     fontWeight = 700,
+  }) => css`
+    font-size: ${theme.sizes[size]};
+    color: ${theme.colors[color]};
+    font-weight: ${fontWeight};
+  `}
+`
+
+
+export const TextButton = styled('span').attrs<IProps>(() => ({
+ 
+}))<IProps>`
+  ${({
+    color = 'white',
+    size = 'xsmall',
+    fontWeight = 500,
   }) => css`
     font-size: ${theme.sizes[size]};
     color: ${theme.colors[color]};
