818940daa5c263a5caf3465f012b6d7efd799b96
MARCO AURELIO <marcocpdti@gmail.com>
2021-07-19 18:54:21 -0300

    add constant theme in file separated

---
+++ public/frontend/src/components/buttonSubmit/constants.ts
@@ -0,0 +1,24 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#111',
+    gray: '#333',
+    red: '#bb3e03',
+    blue: '#0077b6',
+    green: '#25a244'
+  },
+  width: {
+    block: '100%',
+    medium: '75%',
+    xsmall: '1rem',
+  },
+  height: {
+    thin: '30px',
+    medium: '45px',
+    large: '55%'    
+  }
+}
+
+export default {
+  theme, 
+}
