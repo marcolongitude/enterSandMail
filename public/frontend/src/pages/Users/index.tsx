8ced71f0ad283132d675bd9c5aec48e5871e646f
MARCO AURELIO <marcocpdti@gmail.com>
2021-07-27 01:02:48 -0300

    added users list

--- public/frontend/src/pages/Users/index.tsx
+++ public/frontend/src/pages/Users/index.tsx
@@ -2,42 +2,48 @@ import React, { useEffect, useState } from 'react';
 
 import { getAllContacts } from '../../api'
 
-import { ContactWrapper, ContactCard } from './styles';
+import { UserContainer, UserCard, UserList, ButtonWrapper } from './styles';
+
+import { ButtonSubmit } from '../../components/buttonSubmit'
+import { Text } from '../../components/typography'
 
 export const Users = (): JSX.Element => {
 
   const [dataContacts, setDataContacts] = useState([])
+  async function getContacts(token: string) {
+    const contacts: any = await getAllContacts('/users', token)
+    setDataContacts(contacts.data)
+  }
   useEffect(()=> {
-
     const token: any = localStorage.getItem('reduxState')
-
-    async function getContacts() {
-      const contacts: any = await getAllContacts('/users',token)
-      setDataContacts(contacts.data)
-    }
-    
-    getContacts()
-    
+    getContacts(token)
   }, [])
+  
   console.log(dataContacts)
 
   const renderContactsCard = () => {
     return (
       <>
         {dataContacts && dataContacts.map((contact: any, index) => (
-          <ContactCard key={index}>
+          <UserCard key={index}>
             <p >{contact.user_name}</p>
             <p >{contact.user_email}</p>
-          </ContactCard>
+          </UserCard>
         ))}
       </>
     )
   }
 
   return (
-    <ContactWrapper>
-      {renderContactsCard()}
-    </ContactWrapper>  
+    <UserContainer>
+      <Text size="large" color="blue" >Lista de usuários</Text>
+      <ButtonWrapper>
+        <ButtonSubmit width="exsmall" color="blue" type="submit">Adicionar Usuário</ButtonSubmit>
+      </ButtonWrapper>
+      <UserList>
+        {renderContactsCard()}  
+      </UserList>
+    </UserContainer>  
   );
 
 }
