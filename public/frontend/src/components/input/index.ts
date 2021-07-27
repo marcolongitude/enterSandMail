f654e52522efb6b4b4049ec1b201f1874d888a81
MARCO AURELIO <marcocpdti@gmail.com>
2021-07-26 22:08:06 -0300

    add input component

---
+++ public/frontend/src/components/input/index.ts
@@ -0,0 +1,54 @@
+import styled, {css} from 'styled-components'
+import { colors } from '../../styles'
+
+
+interface IProps {
+  width?: keyof typeof theme.width
+  height?: keyof typeof theme.height
+  error: boolean
+}
+
+export const theme = {
+  width: {
+    block: '100%',
+    medium: '75%',
+    xsmall: '1rem',
+  },
+  height: {
+    thin: '30px',
+    medium: '45px',
+    large: '55%'    
+  }
+}
+
+export const Input = styled('input').attrs<IProps>(() => ({
+}))<IProps>`
+  ${({
+    width = 'block',
+  }) => css`
+
+    width: ${theme.width[width]};
+    min-width: 80%;
+    margin: 10px 0 10px 0;
+    background-color: ${colors.backgroundInput};
+    height: 46px;
+    border-radius: 5px;
+    box-sizing: border-box;
+    padding: 10px;
+    border: ${(props: IProps) => props.error ? '1px solid red': `1px solid ${colors.blueActive}`};
+    ::placeholder,
+    ::-webkit-input-placeholder {
+        color: ${colors.placeholder};
+        font-size: 15px;
+    }
+    :-ms-input-placeholder {
+        color: ${colors.placeholder};
+        font-size: 15px;
+    }
+    &:focus {
+        outline: none;
+    }
+
+
+  `}
+`
