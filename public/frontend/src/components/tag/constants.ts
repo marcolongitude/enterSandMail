c49c738c5bd8521773e01a39a2866947bf02a3d6
MARCO AURELIO <marcocpdti@gmail.com>
2021-07-29 19:43:36 -0300

    Add tag component

---
+++ public/frontend/src/components/tag/constants.ts
@@ -0,0 +1,22 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#111',
+    gray: '#333',
+    red: '#bb3e03',
+    blue: '#0077b6',
+    green: '#25a244'
+  },
+  width: {
+    medium: '80px',
+    xsmall: '60px',
+    exsmall: '40px'
+  },
+  height: {
+    thin: '23px',
+    medium: '35px',
+    large: '50px'    
+  }
+}
+
+export default theme 
---
+++ public/frontend/src/components/tag/index.ts
@@ -0,0 +1,35 @@
+import styled, {css} from 'styled-components'
+import { theme } from './constants'
+
+interface IProps {
+  color?: keyof typeof theme.colors
+  width?: keyof typeof theme.width
+  height?: keyof typeof theme.height
+}
+
+export const Tag = styled('div').attrs<IProps>(() => ({}))<IProps>`
+  ${({
+    color = 'blue',
+    width = 'medium',
+    height = 'medium'
+  }) => css`
+    width: ${theme.width[width]};
+    background-color: ${theme.colors[color]};
+    height: ${theme.height[height]};
+    cursor: pointer;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    transition: 0.3s;
+    opacity: 0.8;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    &:hover {
+      opacity: 1.0
+    }
+  `
+  }
+`
+
