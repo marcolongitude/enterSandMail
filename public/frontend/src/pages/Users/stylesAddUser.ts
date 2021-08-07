9bea094859e9083f78c47dcceb4953ec7adb3c8f
MARCO AURELIO <marcocpdti@gmail.com>
2021-08-07 13:29:45 -0300

    add styles add user component

---
+++ public/frontend/src/pages/Users/stylesAddUser.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const ContainerAddUser = styled.div`
+  border: 1px solid #000;
+  margin: 50px;
+  padding: 25px
+`;
+
+export const ContainerTitle = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
+export const ContainerForm = styled.form`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const SideForm = styled.section`
+  width: 45%;
+`;
