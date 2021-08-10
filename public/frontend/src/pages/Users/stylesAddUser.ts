851c96e3ed79230f311644cd6ef397407359e019
MARCO AURELIO <marcocpdti@gmail.com>
2021-08-09 21:27:02 -0300

    refactor styles css

--- public/frontend/src/pages/Users/stylesAddUser.ts
+++ public/frontend/src/pages/Users/stylesAddUser.ts
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
+import { colors } from '../../styles'
 
 export const ContainerAddUser = styled.div`
-  border: 1px solid #000;
-  margin: 50px;
-  padding: 25px
+  background-color: ${colors.backgroundPrimaryCard};
+  -webkit-box-shadow: 3px 5px 11px -5px #000000; 
+  box-shadow: 1px 3px 11px -8px #000000;
+  border-radius: 5px;
+  padding: 15px;
+  margin: 20px;
 `;
 
 export const ContainerTitle = styled.div`
@@ -14,10 +18,12 @@ export const ContainerTitle = styled.div`
 export const ContainerForm = styled.form`
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   justify-content: space-between;
   width: 100%;
 `;
 
 export const SideForm = styled.section`
   width: 45%;
+  min-width: 305px;
 `;
