c97ebc09ace1b6df0dfa50401ff9fdfab205f3e0
MARCO AURELIO <marcocpdti@gmail.com>
2021-08-09 21:26:48 -0300

    Refactor styles css

--- public/frontend/src/components/inputs/select.ts
+++ public/frontend/src/components/inputs/select.ts
@@ -17,7 +17,7 @@ export const Select = styled.select`
   background-image: url("data:image/svg+xml;utf8,<svg fill='black' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
   background-repeat: no-repeat;
   background-position-x: 99%;
-  background-position-y: 5px;
+  background-position-y: 10px;
 
   option {
     color: black;
