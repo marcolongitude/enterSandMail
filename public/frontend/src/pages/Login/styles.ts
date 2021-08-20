422a54392ddef33efa71da5ff9fe7fb13aaa05cd
MARCO AURELIO <marcocpdti@gmail.com>
2021-08-20 20:56:45 -0300

    cleaning styles login page

--- public/frontend/src/pages/Login/styles.ts
+++ public/frontend/src/pages/Login/styles.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { colors } from '../../styles'
 
 export const Container = styled.div`
@@ -72,25 +72,4 @@ export const ButtonSubmit = styled.button`
     }
 `;
 
-interface DotProps {
-    delay: string
-}
-const BounceAnimation = keyframes`
-  0% { margin-bottom: 0; }
-  50% { margin-bottom: 15px }
-  100% { margin-bottom: 0 }
-`;
-export const DotWrapper = styled.div`
-  display: flex;
-  align-items: flex-end;
-`;
-export const Dot = styled.div`
-  background-color: white;
-  border-radius: 50%;
-  width: 6px;
-  height: 6px;
-  margin: 0 5px;
-  /* Animation */
-  animation: ${BounceAnimation} 0.5s linear infinite;
-  animation-delay: ${(props: DotProps) => props.delay};
-`;
+
