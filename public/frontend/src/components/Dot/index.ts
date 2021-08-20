67533cb56cdb37a2bd859bf0033c3c7dea2129a4
MARCO AURELIO <marcocpdti@gmail.com>
2021-08-20 20:55:03 -0300

    add dot animate component

---
+++ public/frontend/src/components/Dot/index.ts
@@ -0,0 +1,27 @@
+import styled, { keyframes } from 'styled-components'
+
+
+interface DotProps {
+  delay: string
+}
+const BounceAnimation = keyframes`
+0% { margin-bottom: 0; }
+50% { margin-bottom: 15px }
+100% { margin-bottom: 0 }
+`;
+
+export const DotWrapper = styled.div`
+display: flex;
+align-items: flex-end;
+`;
+
+export const Dot = styled.div`
+  background-color: white;
+  border-radius: 50%;
+  width: 6px;
+  height: 6px;
+  margin: 0 5px;
+  /* Animation */
+  animation: ${BounceAnimation} 0.5s linear infinite;
+  animation-delay: ${(props: DotProps) => props.delay};
+`;
