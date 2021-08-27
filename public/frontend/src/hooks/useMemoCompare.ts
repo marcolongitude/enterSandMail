df1416e13ceeed87e6accce29a8db593fc9a2846
MARCO AURELIO <marcocpdti@gmail.com>
2021-08-27 11:08:26 -0300

    add useMemoCompare hook

---
+++ public/frontend/src/hooks/useMemoCompare.ts
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef } from "react";
+
+export const useMemoCompare = (next: undefined, compare: (arg0: undefined, arg1: any) => any) => {
+  const previousRef = useRef();
+  const previous = previousRef.current;
+  const isEqual = compare(previous, next);
+
+  useEffect(() => {
+    if (!isEqual) {
+      previousRef.current = next;
+    }
+  });
+
+  return isEqual ? previous : next;
+}
+
+
+// Usage
+// function MyComponent({ obj }) {
+//   const [state, setState] = useState();
+//   // Use the previous obj value if the "id" property hasn't changed
+//   const objFinal = useMemoCompare(obj, (prev, next) => {
+//     return prev && prev.id === next.id;
+//   });
+//   // Here we want to fire off an effect if objFinal changes.
+//   // If we had used obj directly without the above hook and obj was technically a
+//   // new object on every render then the effect would fire on every render.
+//   // Worse yet, if our effect triggered a state change it could cause an endless loop
+//   // where effect runs -> state change causes rerender -> effect runs -> etc ...
+//   useEffect(() => {
+//     // Call a method on the object and set results to state
+//     return objFinal.someMethod().then((value) => setState(value));
+//   }, [objFinal]);
+//   // So why not pass [obj.id] as the dependency array instead?
+//   useEffect(() => {
+//     // Then eslint-plugin-hooks would rightfully complain that obj is not in the
+//     // dependency array and we'd have to use eslint-disable-next-line to work around that.
+//     // It's much cleaner to just get the old object reference with our custom hook.
+//     return obj.someMethod().then((value) => setState(value));
+//   }, [obj.id]);
+//   return <div> ... </div>;
+// }
