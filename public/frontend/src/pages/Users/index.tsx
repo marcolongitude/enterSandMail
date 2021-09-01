e578736c660b2b11a13fcbee0614e4a721b1fa1a
MARCO AURELIO <marcocpdti@gmail.com>
2021-08-31 22:45:08 -0300

    added delete user feature in frontend

--- public/frontend/src/pages/Users/index.tsx
+++ public/frontend/src/pages/Users/index.tsx
@@ -1,45 +1,77 @@
 import React, { useEffect, useState } from 'react';
 
-import { getAllContacts } from '../../api'
+import { getAllContacts, removeUser } from '../../api'
 import { flowService } from "../../helpers/flow"
 
-import { UserContainer, UserCard, UserList, ButtonWrapper, InfoUser } from './styles';
+import { UserContainer, UserCard, UserList, ButtonWrapper, InfoUser, ContainerTags } from './styles';
 
 import { Button } from '../../components/button'
 import { Text, TextButton } from '../../components/typography'
 import { Tag } from '../../components/tag'
 import { ROUTES } from '../../constants';
+import { toast, ToastContainer } from 'react-toastify';
 
 export const Users = (): JSX.Element => {
 
   const [dataContacts, setDataContacts] = useState([])
+
   async function getContacts(token: string) {
     const contacts: any = await getAllContacts('/users', token)
-    setDataContacts(contacts.data)
+    let contactsActive: any = contacts.data.filter((contact: any) => contact.active === 'active')
+    setDataContacts(contactsActive)
   }
   useEffect(()=> {
     const token: any = localStorage.getItem('reduxState')
     getContacts(token)
   }, [])
   
-  console.log(dataContacts)
-
   const redirectAddUser = () => {
     flowService.goTo(ROUTES.ADD_USERS)
+  }
+
+  const handleRemoveUser = async(id_user: number) => {
+    const token: any = localStorage.getItem('reduxState')
+
+    try {
+      const response: any = await removeUser('/users', id_user, token)
+
+      if(response.status === 200) {
+        toast.success(` ${response.status} : Usuário deletado com sucesso`, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+        });
+        getContacts(token)
+      }
+      
+    } catch (error) {
+      
+    }
+
+
+    getContacts(token)
   }
 
   const renderContactsCard = () => {
     return (
       <>
+        <ToastContainer />
         {dataContacts && dataContacts.map((contact: any, index) => (
           <UserCard key={index}>
             <InfoUser>
               <p >{contact.user_name}</p>
               <p >{contact.user_email}</p>
             </InfoUser>
-            <Tag width="medium" height="thin">
-              <TextButton size="tiny" color="white" >Editar</TextButton>
-            </Tag>
+            <ContainerTags>
+              <Tag width="medium" height="thin">
+                <TextButton size="tiny" color="white" >Editar</TextButton>
+              </Tag>
+              <Tag onClick={()=> handleRemoveUser(contact.id_user)} width="medium" height="thin" color="red">
+                <TextButton size="tiny" color="white" >Excluir</TextButton>
+              </Tag>
+            </ContainerTags>
           </UserCard>
         ))}
       </>
