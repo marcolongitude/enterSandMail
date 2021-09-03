d70a4b8ac2186f2073734ecb52e4524912d0f684
MARCO AURELIO <marcocpdti@gmail.com>
2021-09-02 23:50:51 -0300

    add contants in to card

---
+++ public/frontend/src/components/card/constants.ts
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#111',
+    gray: '#333',
+    lightGray: '#E6E7E8',
+    red: '#bb3e03',
+    blue: '#0077b6',
+    lightBlue: '#bfdfff',
+    green: '#25a244'
+  },
+  width: {
+    block: '100%',
+    medium: '75%',
+    xsmall: '50%',
+    exsmall: '30%',
+  },
+  height: {
+    thin: '300px',
+    medium: '400px',
+    large: '500px'    
+  }
+}
