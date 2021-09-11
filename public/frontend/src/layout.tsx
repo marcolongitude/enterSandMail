aaa7766945f53506289552a2b6e6edb503194092
MARCO AURELIO <marcocpdti@gmail.com>
2021-09-11 12:35:55 -0300

    using global layout

--- public/frontend/src/layout.tsx
+++ public/frontend/src/layout.tsx
@@ -1,10 +1,49 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/NavBar/Navbar";
 import GlobalStyle from "./globalStyles";
-// import Footer from './components/Footer'
+import Footer from './components/Footer'
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  /* Flexbox */
+  display: flex;
+  flex-direction: column;
+
+  /* Width & Height */
+  width: 100%;
+  height: calc(100% - 60px);
+
+  /* Misc */
+  overflow: hidden;
+  position: fixed;
+`;
+
+const HeaderWrapper = styled.header`
+  /* Flexbox */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Main = styled.main`
+  /* Width & Height */
+  width: 100%;
+  height: 100%;
+
+  /* Misc */
+  overflow-y: scroll;
+`;
+
+const FooterWrapper = styled.footer``;
+  
+
+
 
 export const Layout = (props: { 
-    children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; 
+    children?: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; 
+    header?: React.ReactNode;
   }) => {
   
   const [isAuth, setIsAuth] = useState(false);
@@ -17,11 +56,18 @@ export const Layout = (props: {
   }, []);
 
   return (
-    <>
+    <Container>
       <GlobalStyle />
       {isAuth && <Navbar />}
-      <div>{props.children}</div>
-    </>
+      <HeaderWrapper>{props.header}</HeaderWrapper>
+      <Main>{props.children}</Main>
+      {isAuth &&
+      <FooterWrapper>
+        <Footer />
+      </FooterWrapper>
+    }
+    </Container>
+
   );
 };
 
