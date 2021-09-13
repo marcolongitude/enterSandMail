9f383d0b9c61e9db5714adc490f740b50c79c5e8
MarcoAurelio <marcocpdti@gmail.com>
2021-09-13 00:35:37 -0300

    clean code

--- public/frontend/src/components/typography/index.tsx
+++ public/frontend/src/components/typography/index.tsx
@@ -10,25 +10,6 @@ interface IProps {
   level?: 1 | 2 | 3 | 4 | 5 | 6
 }
 
-const themes = {
-  colors: {
-    white: '#ffffff',
-    black: '#111',
-    gray: '#333',
-    lightGray: 'CCC',
-    blue: '#0077b6'
-  },
-  sizes: {
-    tiny: '0.7rem',
-    exsmall: '0.9rem',
-    xsmall: '1rem',
-    small: '1.2rem',
-    medium: '1.6rem',
-    large: '2.4rem',
-    xlarge: '3.2rem',
-    xxlarge: '4.0rem'
-  }
-}
 
 export const Text = styled('h1').attrs<IProps>(({ level = 6}) => ({
   as: `h${level}`
