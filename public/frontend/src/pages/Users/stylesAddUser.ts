e054d2d991b6228a647735f7ef05fe877b596f75
MarcoAurelio <marcocpdti@gmail.com>
2021-09-13 00:36:27 -0300

    cleaning code

--- public/frontend/src/pages/Users/stylesAddUser.ts
+++ public/frontend/src/pages/Users/stylesAddUser.ts
@@ -1,18 +1,9 @@
 import styled from 'styled-components'
-import { colors } from '../../styles'
 
-export const ContainerAddUser = styled.div`
-  background-color: ${colors.backgroundPrimaryCard};
-  -webkit-box-shadow: 3px 5px 11px -5px #000000; 
-  box-shadow: 1px 3px 11px -8px #000000;
-  border-radius: 5px;
-  padding: 15px;
-  margin: 20px;
-`;
 
 export const ContainerTitle = styled.div`
-  width: 100%;
-  text-align: center;
+  /* width: 100%;
+  text-align: center; */
 `;
 
 export const ContainerForm = styled.form`
@@ -20,7 +11,7 @@ export const ContainerForm = styled.form`
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-between;
-  width: 100%;
+  padding: 50px;
 `;
 
 export const SideForm = styled.section`
